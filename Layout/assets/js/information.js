371fa985e42690c0359b72adfcf9fe1d8df2d935
tt-thuy <tt-thuy@jit.vn>
2021-12-09 17:24:19 +0700

    js page information

--- Layout/assets/js/information.js
+++ Layout/assets/js/information.js
@@ -1,35 +1,61 @@
 (function ($) {
     $.information = {
         presentTime: '.present-time',
+        jsHoverIcon: '.box-menu',
+        jsIconLogo: '.icon-logo',
+        jsIconOpen: '.open-icon',
+        jsIconClose: '.close-icon',
+        jsIconClick: '.content-information',
+        showDetailInfo: '.detail-title',
 
         _init: function () {
             this.handleEvent();
-            this.onClickIcon();
+            this.hoverIcon();
+            this.changeIcon();
 
         },
         handleEvent: function () {
             var curDate = new Date();
-            // Ngày hiện tại
-            var curDay =  curDate.getDate() < 10 ? "0" + curDate.getDate() : curDate.getDate();
-
-            // Tháng hiện tại
+            var curDay = curDate.getDate() < 10 ? "0" + curDate.getDate() : curDate.getDate();
             var curMonth = curDate.getMonth() + 1;
-
-            // Năm hiện tại
             var curYear = curDate.getFullYear();
-
-            // Giờ hiện tại
             var hours = curDate.getHours() < 10 ? "0" + curDate.getHours() : curDate.getHours();
-
-            //Phút hiện tại
-            var minutes =  curDate.getMinutes() < 10 ? "0" + curDate.getMinutes() : curDate.getMinutes();
+            var minutes = curDate.getMinutes() < 10 ? "0" + curDate.getMinutes() : curDate.getMinutes();
 
             $(".present-time").text(curYear + "/" + curMonth + "/" + curDay);
-            $(".current-time").text( hours + ":" + minutes);
+            $(".current-time").text(hours + ":" + minutes);
         },
-        onClickIcon: function (){
+        hoverIcon: function () {
+            var el = this;
+            $(el.jsHoverIcon).hover(function () {
+                //console.log('test');
+                var imgHover = $(this).find('.img-hover').attr("src");
+                $(this).find('.img-home').attr("src", imgHover);
+            }, function () {
+                var imgHover = $(this).find('.img-default').attr("src");
+                $(this).find('.img-home').attr("src", imgHover);
+            });
+        },
+
+        changeIcon: function () {
+            var el = this;
+            $(el.jsIconClick).click(function () {
+                var imgOpen = $(this).find('.close-icon').attr("src");
+                console.log(imgOpen,'close');
+                $(this).find('.open-icon').attr("src", imgOpen);
+                console.log('open');
+                $(this).find('.detail-title').slideDown('show');
+
+            });
+                // $(el.jsIconClick).click(function () {
+                // console.log('test');
+                // var imgClose = $(this).find('.open-icon-default').attr("src");
+                // $(this).find('.open-icon').attr("src", imgClose);
+                // $(this).find('.detail-title').slideUp();
+            // });
 
         }
+
     }
 })(jQuery);
 $(document).ready(function () {
